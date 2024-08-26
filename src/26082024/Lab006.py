1298400ea043750548ac0083aae3a68d0ec5f776
Smita <agaresmita@gmail.com>
2024-08-26 13:28:09 +0530

    All files till 26th auguest

---
+++ src/26082024/Lab006.py
@@ -0,0 +1,7 @@
+a=20
+b=50
+c=70
+sum = a+b+c
+sum = sum-11
+sum = sum+15
+print(sum)
---
+++ src/ex_21082024/Lab001.py
@@ -0,0 +1,5 @@
+# My first code in python
+
+print("Hello", "Smita", "welcome to python program", sep='--', end="  ")
+
+print("smita")
